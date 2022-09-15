79ed98be18ee6b687662699109d56a437425cc07
hydrandt <hydrandt@aiya.cz>
2022-09-15 19:36:27 +0200

    adding upvoting and sorting in gallery

--- pages/gallery.js
+++ pages/gallery.js
@@ -3,19 +3,57 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.scss";
-import { Container, Row, Card, Modal } from "react-bootstrap";
+import { Container, Row, Card, Modal, ButtonGroup, ToggleButton } from "react-bootstrap";
 import Col from "react-bootstrap/Col";
-import { gql, useQuery } from "@apollo/client";
+import { gql, useQuery, useMutation } from "@apollo/client";
 import Lightbox from "react-18-image-lightbox";
 import "react-18-image-lightbox/style.css";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { DiscussionEmbed, CommentCount } from 'disqus-react';
+import HeartIcon from 'mdi-react/HeartIcon';
+import HeartOutlineIcon from 'mdi-react/HeartOutlineIcon';
 
 // graphql api client
 const client = new ApolloClient({
   uri: "https://gruenaufkumpelin.de/graphql",
   cache: new InMemoryCache(),
 });
+
+// query for all screenshots
+const ALL_SCREENSHOTS_QUERY = gql`
+  query allScreenshots($sortBy: [String]) {
+    screenshots(sort: $sortBy) {
+      data {
+        id
+        attributes {
+          url
+          authorName
+          createdAt
+          upvoteCount
+        }
+      }
+    }
+  }
+`;
+
+// mutation to upvote a screenshot
+const UPVOTE_SCREENSHOT_MUTATION = gql`
+  mutation upvoteScreenshot($id: ID!, $upvoteCount: Int) {
+    updateScreenshot(
+      id: $id
+      data: {
+        upvoteCount: $upvoteCount
+      }
+    ) {
+      data {
+        id
+        attributes {
+          upvoteCount
+        }
+      }
+    }
+  }
+`;
 
 // image component that can maximise the image
 const ImageLightbox = ({ url, className }) => {
@@ -38,7 +76,7 @@ const ImageLightbox = ({ url, className }) => {
   );
 };
 
-// disqus modal
+// disqus modal component
 const DisqusModal = ({
   disqusModalOpen,
   setDisqusModalOpen,
@@ -48,7 +86,8 @@ const DisqusModal = ({
     <Modal
       show={disqusModalOpen}
       onHide={() => setDisqusModalOpen(false)}
-      style={{ padding: 0, borderRadius: "10px" }}>
+      style={{ padding: 0, borderRadius: "10px" }}
+      size="lg">
       <Card>
         <ImageLightbox
           url={disqusModalPayload.url}
@@ -72,8 +111,8 @@ const DisqusModal = ({
         />
 
         <div className="text-center mt-3">
-          <button className="btn btn-outline-primary" onClick={() => setDisqusModalOpen(false)}>
-            Close
+          <button className="btn btn-danger" onClick={() => setDisqusModalOpen(false)}>
+            Schließen
           </button>
         </div>
       </Modal.Body>
@@ -81,27 +120,109 @@ const DisqusModal = ({
   );
 }
 
-// query
-const ALL_SCREENSHOTS_QUERY = gql`
-  query allScreenshots {
-    screenshots(sort: "createdAt:DESC") {
-      data {
-        id
-        attributes {
-          url
-          authorName
-          createdAt
-          upvoteCount
-        }
-      }
-    }
-  }
-`;
+// single screenshot card component
+const SingleScreenshotCard = ({
+  screenshotPayload,
+  index,
+  setDisqusModalOpen,
+  setDisqusModalPayload
+}) => {
+  const [upvoteCount, setUpvoteCount] = useState(screenshotPayload.attributes.upvoteCount);
+  const [canUpvote, setCanUpvote] = useState(true);
+  // upvote mutation
+  const [upvoteScreenshot] = useMutation(UPVOTE_SCREENSHOT_MUTATION, {
+    client: client
+  });
+
+  return (
+    <Col
+      className='col-12 col-sm-4 gallery-pic'
+      key={screenshotPayload.attributes.url + index}>
+      <Card className='ml-4'>
+        <div className='position-relative'>
+          <ImageLightbox
+            url={screenshotPayload.attributes.url}
+            className='responsive border-0 card-img-top'
+          />
+        </div>
+        <Card.Body className='pb-3 pt-3'>
+          <Row>
+            <Col>
+              <div className='font-weight-bold h5'>
+                {screenshotPayload.attributes.authorName}
+              </div>
+              <div className='text-normal align-self-center'>
+                {Intl.DateTimeFormat(navigator.language, {
+                  weekday: "long",
+                  hour: "numeric",
+                  minute: "numeric",
+                }).format(new Date(screenshotPayload.attributes.createdAt))}
+              </div>
+              <Link legacyBehavior={false} href="#" onClick={() => {
+                setDisqusModalOpen(true);
+                setDisqusModalPayload({
+                  url: screenshotPayload.attributes.url,
+                  identifier: screenshotPayload.attributes.url,
+                  title: screenshotPayload.attributes.authorName,
+                });
+              }}>
+                <CommentCount
+                  shortname='gruenaufkumpelin'
+                  config={
+                    {
+                      url: screenshotPayload.attributes.url,
+                      identifier: screenshotPayload.attributes.url,
+                      title: screenshotPayload.attributes.authorName,
+                    }
+                  }
+                >
+                  {/* Placeholder Text */}
+                  Comments
+                </CommentCount>
+              </Link>
+            </Col>
+            <Col>
+              <div className="text-end">
+                <span className="fs-5 align-bottom">{upvoteCount && upvoteCount + ' '}</span>
+                <span className="align-baseline">
+                  <Link legacyBehavior={false} href="#" onClick={() => {
+                    if (canUpvote) {
+                      upvoteScreenshot({
+                        variables: {
+                          id: screenshotPayload.id,
+                          upvoteCount: upvoteCount + 1,
+                        },
+                      }).then(() => {
+                        setUpvoteCount(upvoteCount + 1);
+                        setCanUpvote(false);
+                      });
+                    }
+                  }}>
+                    {canUpvote ? <HeartOutlineIcon size={30} style={{ color: '#FF7F7F' }} /> : <HeartIcon size={30} style={{ color: "#FF7F7F" }} />}
+                  </Link>
+                </span>
+              </div>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
 
 // whole page component
 export default function Home() {
+  // disqus modal open/closed
+  const [disqusModalOpen, setDisqusModalOpen] = useState(false);
+  const [disqusModalPayload, setDisqusModalPayload] = useState({});
+
+  // decide how we sort
+  const [sortByUpvotes, setSortByUpvotes] = useState(false);
   // query
   const { loading, data } = useQuery(ALL_SCREENSHOTS_QUERY, {
+    variables: {
+      sortBy: sortByUpvotes ? ["upvoteCount:DESC", "createdAt:DESC"] : ["createdAt:DESC"]
+    },
     client: client,
     pollInterval: 3000,
     fetchPolicy: 'network-only',
@@ -109,82 +230,60 @@ export default function Home() {
     fetchPolicy: 'no-cache',
   });
 
-  // disqus modal open/closed
-  const [disqusModalOpen, setDisqusModalOpen] = useState(false);
-  const [disqusModalPayload, setDisqusModalPayload] = useState({});
-
-
-  if (loading) {
-    return <div></div>;
-  }
-
   return (
     <div className={styles.gallery}>
       <Head>
         <link rel='manifest' href='/manifest.json' />
         <link rel='apple-touch-icon' href='/icon-192x192.png' />
         <meta name='theme-color' content='#ffed00' />
-        <title>Gallery</title>
+        <title>Galerie</title>
         <meta
           name='description'
-          content='Controll the VR glasses for the Biennale lala.ruhr 2022'
+          content='Galerie: the Biennale lala.ruhr 2022'
         />
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <h1 className={styles.title}>Galerie</h1>
-      <Container>
-        <Row>
-          {data.screenshots.data.map((singleScreenshot, index) => (
-            <Col
-              className='col-12 col-sm-4 gallery-pic'
-              key={singleScreenshot.attributes.url + index}>
-              <Card className='ml-4'>
-                <div className='position-relative'>
-                  <ImageLightbox
-                    url={singleScreenshot.attributes.url}
-                    className='responsive border-0 card-img-top'
-                  />
-                </div>
-                <Card.Body className='pb-3 pt-3'>
-                  <div className='font-weight-bold h5'>
-                    {singleScreenshot.attributes.authorName}
-                  </div>
-                  <div className='text-normal align-self-center'>
-                    {Intl.DateTimeFormat(navigator.language, {
-                      weekday: "long",
-                      hour: "numeric",
-                      minute: "numeric",
-                    }).format(new Date(singleScreenshot.attributes.createdAt))}
-                  </div>
-<Link legacyBehavior={false} href="#" onClick={() => {
-                      setDisqusModalOpen(true);
-                      setDisqusModalPayload({
-                        url: singleScreenshot.attributes.url,
-                        identifier: singleScreenshot.attributes.url,
-                        title: singleScreenshot.attributes.authorName,
-                      });
-                    }}>
-                      <CommentCount
-                        shortname='gruenaufkumpelin'
-                        config={
-                          {
-                            url: singleScreenshot.attributes.url,
-                            identifier: singleScreenshot.attributes.url,
-                            title: singleScreenshot.attributes.authorName,
-                          }
-                        }
-                      >
-                        {/* Placeholder Text */}
-                        Comments
-                      </CommentCount>
-                    </Link>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </Container>
+      <h1 className="mt-4 text-center">Galerie</h1>
+      <div className="text-center">
+      <ButtonGroup className="mb-3">
+        <ToggleButton
+          type="radio"
+          id="latest"
+          variant={ !sortByUpvotes ? 'danger' : 'secondary' }
+          style={{ marginRight: 0, boxShadow: 'none', minWidth: "150px" }}
+          name="radio"
+          onChange={(e) => setSortByUpvotes(false)}
+        >
+          Neueste
+        </ToggleButton>
+        <ToggleButton
+          type="radio"
+          id="popular"
+          variant={ sortByUpvotes ? 'danger' : 'secondary' }
+          style={{ boxShadow: 'none', minWidth: "150px"}}
+          name="radio"
+          onChange={(e) => setSortByUpvotes(true)}
+        >
+          Beliebt
+        </ToggleButton>
+
+      </ButtonGroup>
+      </div>
+
+      {!loading &&
+        <Container>
+          <Row>
+            {data.screenshots.data.map((singleScreenshot, index) => (
+              <SingleScreenshotCard
+                screenshotPayload={singleScreenshot}
+                setDisqusModalOpen={setDisqusModalOpen}
+                setDisqusModalPayload={setDisqusModalPayload}
+                index={index} />
+            ))}
+          </Row>
+        </Container>
+      }
       <DisqusModal
         disqusModalOpen={disqusModalOpen}
         setDisqusModalOpen={setDisqusModalOpen}
