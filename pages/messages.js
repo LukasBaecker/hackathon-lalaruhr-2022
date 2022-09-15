864aa77b0435720da91cb0aeadf114ecc750edab
LukasBaecker <baecker.lukas@outlook.de>
2022-09-15 22:18:08 +0200

    styling message page

--- pages/messages.js
+++ pages/messages.js
@@ -3,15 +3,23 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.scss";
-import { Container, Row, Card, Modal, ButtonGroup, ToggleButton } from "react-bootstrap";
+import Alert from "react-bootstrap/Alert";
+import {
+  Container,
+  Row,
+  Card,
+  Modal,
+  ButtonGroup,
+  ToggleButton,
+} from "react-bootstrap";
 import Col from "react-bootstrap/Col";
 import { gql, useQuery, useMutation } from "@apollo/client";
 import Lightbox from "react-18-image-lightbox";
 import "react-18-image-lightbox/style.css";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { DiscussionEmbed, CommentCount } from 'disqus-react';
-import HeartIcon from 'mdi-react/HeartIcon';
-import HeartOutlineIcon from 'mdi-react/HeartOutlineIcon';
+import { DiscussionEmbed, CommentCount } from "disqus-react";
+import HeartIcon from "mdi-react/HeartIcon";
+import HeartOutlineIcon from "mdi-react/HeartOutlineIcon";
 
 // graphql api client
 const client = new ApolloClient({
@@ -35,37 +43,33 @@ const ALL_MESSAGES_QUERY = gql`
 `;
 
 // single screenshot card component
-const SingleMessageCard = ({
-  createdAt,
-  messageText,
-  index
-}) => {
+const SingleMessageCard = ({ createdAt, messageText, index }) => {
   return (
-    <div key={index}>
+    <Alert key={index} variant='primary' style={{ textAlign: "left" }}>
       <h3>
         {messageText}
-        {' ['}
+        {" ["}
         {Intl.DateTimeFormat(navigator.language, {
           hour: "numeric",
           minute: "numeric",
           second: "numeric",
         }).format(new Date(createdAt))}
-        {'] '}</h3>
-    </div>
+        {"] "}
+      </h3>
+    </Alert>
   );
 };
 
 // whole page component
 export default function Home() {
-
   const { loading, data } = useQuery(ALL_MESSAGES_QUERY, {
     client: client,
     pollInterval: 1000,
-    fetchPolicy: 'network-only',
+    fetchPolicy: "network-only",
   });
 
   if (loading) {
-    return <></>
+    return <></>;
   }
 
   return (
@@ -82,24 +86,18 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <h1 className="mt-4 text-center">Messages</h1>
-      <div className="text-center">
-
-
-        
-          <Container>
-
-            {data.messages.data.map((singleMessage, index) => (
-              <SingleMessageCard
-                messageText={singleMessage.attributes.messageText}
-                createdAt={singleMessage.attributes.createdAt}
-                index={index}
-                key={index} />
-            ))}
-
-          </Container>
-        
-
+      <h1 className='mt-4 text-center'>Messages</h1>
+      <div className='text-center'>
+        <Container>
+          {data.messages.data.map((singleMessage, index) => (
+            <SingleMessageCard
+              messageText={singleMessage.attributes.messageText}
+              createdAt={singleMessage.attributes.createdAt}
+              index={index}
+              key={index}
+            />
+          ))}
+        </Container>
       </div>
     </div>
   );
